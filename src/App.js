d01b8faf1bca1fe62440615b9e9dfe05b86b09ac
dknott2602 <dknott2602@gmail.com>
2025-06-03 11:46:23 -0700

    commit message

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import Nav from './components/Nav';
 import Footer from './components/Footer';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './pages/Home';
+import { books } from "./data";
+import Books from './pages/Books';
+
+
+
 
 
 function App() {
@@ -9,8 +14,9 @@ function App() {
     <Router>
 
     <div className="App">
-      <Home/>
       <Nav/>
+      <Route path="/" exact component={Home} />
+      <Route path="/books" render={() => <Books books={books} />}/>
       <Footer/>
     </div>
     </Router>
